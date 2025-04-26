8c632bdf4f98c7aed53279f51b86dfb6d36d4633
fisayo-dev <olufisayobadina@gmail.com>
2025-04-26 08:07:12 +0100

    feat: remove not-found code block

--- app/(dashboard)/courses/[courseId]/page.tsx
+++ app/(dashboard)/courses/[courseId]/page.tsx
@@ -2,7 +2,6 @@ import { auth } from "@/auth";
 import LecturerCoursePage from "@/components/lecturer/LecturerCourseHome";
 import StudentCoursePage from "@/components/student/CourseHome";
 import { getCourseById } from "@/lib/actions/course.action";
-import { notFound } from "next/navigation";
 
 const Page = async ({ params }: any) => {
   const { courseId } = params;
@@ -10,9 +9,7 @@ const Page = async ({ params }: any) => {
   const role = session?.user?.role;
   const isStudent = role === "STUDENT";
   const course = await getCourseById(courseId);
-  if (!course) {
-    return notFound();
-  }
+ 
   console.log("course", course);
   return (
     <>
