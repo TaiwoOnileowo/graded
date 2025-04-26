ca522bd052fbcc48b5b108ea98a6b0dc50979037
fisayo-dev <olufisayobadina@gmail.com>
2025-04-26 12:59:15 +0100

    chore: get assignment Id in editor route

--- app/(dashboard)/courses/[courseId]/assignments/[assignmentId]/editor/page.tsx
+++ app/(dashboard)/courses/[courseId]/assignments/[assignmentId]/editor/page.tsx
@@ -1,7 +1,8 @@
-const page = () => {
+const Page = async ({ params }: any) => {
+    const {assignmentId} = params;
   return (
-    <div>Assignment Editor</div>
+    <div>Assignment Editor for assignment {assignmentId}</div>
   )
 }
 
-export default page
+export default Page
