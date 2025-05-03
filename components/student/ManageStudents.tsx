a420f0dced0af996e735ab3e7ebaacd7c4237eb0
fisayo-dev <olufisayobadina@gmail.com>
2025-05-03 17:54:54 +0100

    chore: layout - manage students

--- components/student/ManageStudents.tsx
+++ components/student/ManageStudents.tsx
@@ -1,7 +1,21 @@
-const ManageStudents = ({ courseId} : { courseId: string}) => {
-    return (
-      
-    <div>ManageStudents</div>
+import { Button } from "../ui/button"
+import { ArrowLeft } from "lucide-react"
+import Link from "next/link"
+const ManageStudents = ({ courseId }: { courseId: string }) => {
+    return (    
+        <div className="space-y-6">
+            <div className="flex items-center gap-2">
+                <Button variant="ghost" size="icon" asChild>
+                    <Link href={`/courses/${courseId}`}>
+                    <ArrowLeft className="h-4 w-4" />
+                    <span className="sr-only">Back</span>
+                    </Link>
+                </Button>
+                <h2 className="text-2xl font-bold tracking-tight">
+                    Manage Students
+                </h2>
+            </div>
+        </div>
   )
 }
 
