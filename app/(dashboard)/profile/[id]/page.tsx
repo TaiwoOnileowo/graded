4c22a5237917806c43e9b933abfd72beea0f1a8d
fisayo-dev <olufisayobadina@gmail.com>
2025-05-07 00:39:12 +0100

    chore get profile data from session & populate the  profile data

--- app/(dashboard)/profile/[id]/page.tsx
+++ app/(dashboard)/profile/[id]/page.tsx
@@ -1,8 +1,31 @@
-const page = ({ params } : any) => {
+import { auth } from "@/auth";
+import ProfileCard from "@/components/profile/ProfileCard";
+
+const page = async ({ params } : any) => {
     const { id } = params; 
-    const student
+    // Get session
+    const session = await auth()
+
+    // Detect role
+    const role = session?.user?.role;
+    const isStudent = role === "STUDENT";
+
+    // Gettoing the profile data
+    let profilePerson;
+    if (isStudent) {
+        profilePerson = session?.user;
+    } else {
+        profilePerson = session?.user;
+    }
+
+    const profileData = {
+        name: profilePerson?.name,
+        email: profilePerson?.email,
+        image: profilePerson?.image
+    }
+
   return (
-    <div>{id}</div>
+    <ProfileCard personDetails={profileData}/>
   )
 }
 
--- components/profile/ProfileCard.tsx
+++ components/profile/ProfileCard.tsx
@@ -3,21 +3,21 @@ import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 interface ProfileDetails {
     name: string;
     email: string;
-    createdAt: Date;
+    createdAt?: Date;
     image?: string
 }
-const ProfileCard = ({ studentDetails} : {studentDetails: ProfileDetails }) => {
+const ProfileCard = ({ personDetails} : { personDetails: ProfileDetails }) => {
   return (
     <div className="w-full md:min-w-2xl rounded-2xl shadow-lg bg-white overflow-hidden mx-auto">
       <div className="grid space-y-4">
         <Avatar className="w-32 h-32 mx-auto">
-          <AvatarImage src={studentDetails.image} alt={studentDetails.name} />
+          <AvatarImage src={personDetails.image} alt={personDetails.name} />
           <AvatarFallback className="text-lg font-bold">
-            {studentDetails.name?.charAt(0).toUpperCase() || "U"}
+            {personDetails.name?.charAt(0).toUpperCase() || "U"}
           </AvatarFallback>
         </Avatar>
-        <h2 className="text-2xl">{studentDetails.name}</h2>
-        <span className="text-sm">{studentDetails.email}</span>
+        <h2 className="text-2xl">{personDetails.name}</h2>
+        <span className="text-sm">{personDetails.email}</span>
       </div>
     </div>
   )
