0d68003b9fd387f589e18a1f919d2fdd476ce7f8
fisayo-dev <olufisayobadina@gmail.com>
2025-05-07 15:44:55 +0100

    feat: add seo to single course page

--- app/(dashboard)/courses/[courseId]/page.tsx
+++ app/(dashboard)/courses/[courseId]/page.tsx
@@ -1,18 +1,43 @@
 import { auth } from "@/auth";
 import LecturerCoursePage from "@/components/lecturer/LecturerCourseHome";
 import StudentCoursePage from "@/components/student/CourseHome";
-import { getCourseById } from "@/lib/actions/course.action";
+import { getCourseById, getCourseName } from "@/lib/actions/course.action";
 import { notFound } from "next/navigation";
+import type { Metadata } from "next";
 
-const Page = async ({ params }: any) => {
-  const { courseId } = await params;
+export async function generateMetadata({ params }: any): Promise<Metadata> {
+  const { courseId } = params;
   const session = await auth();
   const role = session?.user?.role;
   const isStudent = role === "STUDENT";
   const course = await getCourseById(courseId);
+  const { name, code } = await getCourseName(courseId);
+
+  if (!course) {
+    return {};
+  }
+
+  return {
+    title: isStudent
+      ? `Your Course: ${name} (${code})`
+      : `Manage Course: ${name} (${code})`,
+    description: isStudent
+      ? `Access all your learning materials and updates for ${name} (${code}).`
+      : `Manage content, materials, and students for the course ${name} (${code}).`,
+  };
+}
+
+const Page = async ({ params }: any) => {
+  const { courseId } = params;
+  const session = await auth();
+  const role = session?.user?.role;
+  const isStudent = role === "STUDENT";
+  const course = await getCourseById(courseId);
+
   if (!course) {
     return notFound();
   }
+
   return (
     <>
       {isStudent ? (
@@ -23,4 +48,5 @@ const Page = async ({ params }: any) => {
     </>
   );
 };
+
 export default Page;
